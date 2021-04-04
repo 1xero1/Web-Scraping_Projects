2fa36910e7c2c99181b83e58db4d087c3fb7ca1a
1xero1 <1xero1@users.noreply.github.com>
2021-04-04 10:58:16 +0500

    Clean the code, and update the comments

--- Bank-Notes-Web-Scrapper.py
+++ Bank-Notes-Web-Scrapper.py
@@ -1,4 +1,4 @@
-#Extract bank notes details from the website ''
+###Extract bank notes details from the website ''
 
 import requests
 from bs4 import BeautifulSoup
@@ -14,14 +14,14 @@ lst=[]
 
 BaseURL=""
 BasePage = requests.get('')
-BaseStatus = BasePage.status_code #check if the website working status
+BaseStatus = BasePage.status_code #check if the website working
 if BaseStatus==200:
     print("Success! The Main page is working")
 
-BasePageSoup = BeautifulSoup(BasePage.content, 'html.parser')#get the page content via 'html.parser'
+BasePageSoup = BeautifulSoup(BasePage.content, 'html.parser')
 #Since our landing page has href links leading to final extraction destination
 #we extract all links tagged with href
-lst=[]
+
 BaseSoupFind=BasePageSoup.find_all('a')
 
 for link in BaseSoupFind:
@@ -29,18 +29,16 @@ for link in BaseSoupFind:
     lst.append(url)
 #lst
 
-LangSelect=lst[4:16] # List for language selection
-CountriesURLsList=lst[17:-25] #List of Countries (destinatiopn URLs)
-#CountriesURLsList
+LangSelect=lst[4:16] # List for language selection (Optional Implementation)
+CountriesURLsList=lst[17:-25] #List of Countries (destination URLs)
 
 # List of country names to save images in their respective folders
-CountryNames=[]
+
 for nm in CountriesURLsList:
     nam=nm.split("/")
     Name=nam[-1]
     CountryName=Name[0:-5]
     CountryNames.append(CountryName)
-    #CountryNames
 
 for CountryURL in CountriesURLsList:
     CountryPageURL=BaseURL+CountryURL
@@ -55,7 +53,7 @@ for CountryURL in CountriesURLsList:
         ImgList.append(src)
 print("All Images URLs retrieved")
     
-#Remove Logo Images
+#Remove extra Logo Images
 Logo=
 Logo1=
 while Logo in ImgList:
@@ -63,7 +61,7 @@ while Logo in ImgList:
 while Logo1 in ImgList:
     ImgList.remove(Logo1)
 
-#Get teh Full Image URLS    
+#Get the Full Image URLS    
 for url in ImgList:
     ImgURL=url[3:]
     ImageURL=BaseURL+ImgURL
@@ -77,10 +75,12 @@ for URL in ImURL:
     ImageName.append(ImgName)
 print("Image names retrieved from the Image URLs")
 
-# Write Images
+# Write Images to local storage
 for x in range(0,len(ImURL)):
     img_data = requests.get(ImURL[x]).content
     with open(ImageName[x], 'wb') as handler:
         handler.write(img_data)
         print(ImageName[x], "at: %s" % ImURL[x], "is Saved")
 print("All images have been saved")
+
+#***********************x*x*x****************************#
